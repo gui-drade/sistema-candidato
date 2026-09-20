38404961c82b8255b06aff885078f4e90a2b4cb2
Guilherme de Andrade <guilhermeandradebarbo112@gmail.com>
2026-09-20 17:27:59 -0300

    criação da enum
    criação de uma enum para os poderes e as esferas.

---
+++ src/Enum.ts
@@ -0,0 +1,11 @@
+ export enum Esfera  {
+    Municipal = "Municipal",
+    Estadual = "Estadual",
+    Federal = "Federal"
+}
+
+ export enum Poder  {
+    Executivo = "Executivo",
+    Legislativo = "Legislativo",
+    Judiciario = "Judiciário"   
+}

e7852a2bf68fbf5ff907643b62128ef5257baff4
Guilherme de Andrade <guilhermeandradebarbo112@gmail.com>
2026-09-21 17:32:27 -0300

    finalização e correção
    finalizando e corrigindo erros de logica em presidente

--- src/presidente.ts
+++ src/presidente.ts
@@ -6,9 +6,61 @@ class Presidente extends Politico {
 
 
 
-    constructor(ministros:string[],nome: string, partido:string, esfera:Esfera,remuneracao: number,poder: Poder, projetos:string[],nomeTrabalho: string, enderecoTrabalho: string) {
+    constructor(nome: string, partido:string, esfera:Esfera,remuneracao: number,poder: Poder, projetos:string[],nomeTrabalho: string, enderecoTrabalho: string,ministros:string[]) {
         super(nome,partido,esfera,remuneracao,poder,projetos,nomeTrabalho,enderecoTrabalho)
         this.ministros = ministros
 
     }
+
+   comandarForcasArmadas(): string {
+    return "Comanda forças armadas"
+   }
+
+   exercerMandato(): string {
+    return "O presidente exerce o mandato propondo, sancionando ou vetando leis e editando medidas provisórias."
+   }
+
+   nomearMinistro(ministroNovo: string): string {
+           this.ministros.push(ministroNovo)
+           return 'O presidente nomeou ' + ministroNovo + ' como ministro';
+
+   }
+   exonerarMinistro(ministroExonerado:string): string {
+    if (this.ministros.length === 0) {
+       return 'não há ministro para ser exonerado'
+      
+    }
+      const existeMinistro = this.ministros.some(
+        ministro => ministro === ministroExonerado
+      )
+       if(existeMinistro === true) {
+        this.ministros = this.ministros.filter(ministro => ministro !== ministroExonerado)
+        return `O ministro ${ministroExonerado} foi exonerado`
+      }
+
+      else {
+        return `O ministro ${ministroExonerado} não pode ser encontrado`
+      }
+
+   
+   }
+   representarPais(evento:string): string {
+    return  `O presidente ${this.nome} foi representar o pais em: ` + evento
+   }
+
+   elaborarPPA(PPA: string): string {
+    return `O presidente elaborou um PPA ${PPA}`
+   }
+
+    elaborarLDO(LDO: string): string {
+    return `O presidente elaborou um LDO ${LDO}`
+   }
+
+    elaborarLOA(LOA: string): string {
+    return `O presidente elaborou um LOA ${LOA}`
+   }
+
+
+
+
 }
